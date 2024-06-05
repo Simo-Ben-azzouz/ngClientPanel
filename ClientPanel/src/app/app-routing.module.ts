204f9324069a8331ac989b2a2c7e0d1f7b4ae3fc
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-06-05 23:35:12 +0100

    add routing for navigate

--- ClientPanel/src/app/app-routing.module.ts
+++ ClientPanel/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DetailsClientComponent } from './components/details-client/details-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "" , component : DashboardComponent},
+  {path : "login" , component : LoginComponent},
+  {path : "register" , component : RegisterComponent},
+  {path : "client/add" , component : AddClientComponent},
+  {path : "client/edit:id" , component : EditClientComponent},
+  {path : "client/:id" , component : DetailsClientComponent},
+  {path : "settings" , component :SettingsComponent},
+  {path : "**" , component : NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
