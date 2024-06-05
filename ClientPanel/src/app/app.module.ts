94dd8fc14106349b82abdaef0e8c839f959c7865
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-06-05 23:29:43 +0100

    add componenets

--- ClientPanel/src/app/app.module.ts
+++ ClientPanel/src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { ClientComponent } from './components/client/client.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DetailsClientComponent } from './components/details-client/details-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    AddClientComponent,
+    ClientComponent,
+    DashboardComponent,
+    DetailsClientComponent,
+    EditClientComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    SettingComponent,
+    SettingsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
