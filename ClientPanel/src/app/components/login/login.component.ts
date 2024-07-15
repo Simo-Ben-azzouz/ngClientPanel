f889821406da03a98f2b24dcaa259ca5b6bcb5eb
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-07-15 05:59:09 +0100

    authentication with email and password (134)

--- ClientPanel/src/app/components/login/login.component.ts
+++ ClientPanel/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthClientService } from '../../services/auth-client.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -8,4 +11,35 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   email! : string;
   password! : string;
+  /**
+   *
+   */
+  constructor(
+    private authService : AuthClientService,
+    private route : Router,
+    private toastr : ToastrService
+  ) {}
+
+  ngOnInit(){
+    this.authService.getAuth().subscribe(
+      (auth) =>{
+        if (auth) {
+          this.route.navigate(['/']);
+        }
+      }
+    )
+  }
+  // methode 
+  onLogin(){
+    this.authService.Login(this.email , this.password)
+    .then((auth) =>{
+      if (auth) {
+        this.toastr.success('You are logged succesfuly');
+        this.route.navigate(['/']);
+      }
+    })
+    .catch((error) =>{
+      this.toastr.show(error);
+    })
+  }
 }
--- ClientPanel/src/app/services/auth-client.service.ts
+++ ClientPanel/src/app/services/auth-client.service.ts
@@ -1,9 +1,26 @@
+import { auth } from 'firebase';
+import { map } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthClientService {
 
-  constructor() { }
+  constructor(private afAuth : AngularFireAuth) { }
+
+  // methode
+  Login(email : string , password : string){
+    return new Promise((resolve , reject) =>{
+      this.afAuth.signInWithEmailAndPassword(email , password)
+      .then((UserData) => resolve(UserData) , (error) => reject(error) )
+    })
+  }
+
+  getAuth(){
+    return this.afAuth.authState.pipe(
+      map(auth => auth)
+    )
+  }
 }
